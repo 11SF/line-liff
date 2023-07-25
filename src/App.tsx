e0c5bac8fefe9cb14af6c1fc6be472982c3aed11
Nawaphong Sitaruno <nawaphong.s@arise.tech>
2023-07-25 13:27:45 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Profile } from "@liff/get-profile";
 import liff from "@line/liff";
 import { useEffect, useState } from "react";
@@ -7,10 +8,34 @@ function App() {
   const [user, setUser] = useState<Profile | null>(null);
 
   useEffect(() => {
-    liff.getProfile().then((r) => {
-      setUser(r);
-    });
+    liff
+      .init({
+        liffId: "2000214509-NzW3XVOX", // Use own liffId
+      })
+      .then(() => {
+        if (liff.isLoggedIn()) {
+          liff.getProfile().then((r) => {
+            setUser(r);
+            console.log(r);
+          });
+        } else {
+          liff.login();
+        }
+      })
+      .catch((err) => {
+        throw err;
+      });
   }, []);
+
+  // useEffect(() => {
+  //   if (liff.isLoggedIn()) {
+  //     liff.getProfile().then((r) => {
+  //       setUser(r);
+  //     });
+  //   } else {
+  //     liff.login();
+  //   }
+  // }, []);
 
   return <div>{user !== null ? <h1>Hello {user.displayName}</h1> : ""}</div>;
 }
